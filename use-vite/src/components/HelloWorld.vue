3072414bd638f6ae332c7639ae39c572587758df
Alex Zhang <alexzhangyaotian@gmail.com>
2024-06-29 13:59:21 +0800

    feat:responsive-img

--- use-vite/src/components/HelloWorld.vue
+++ use-vite/src/components/HelloWorld.vue
@@ -1,39 +1,24 @@
 <script setup>
-import { ref } from 'vue'
+import { ref,computed } from 'vue'
 
 defineProps({
   msg: String,
 })
 
-const count = ref(0)
+const count = ref(0);
+const imgUrls = ['https://p8.itc.cn/images01/20230824/7d2c939956664f3c86ffb564f33aa57c.jpeg','https://live.staticflickr.com/2511/4142792553_4601d0fbdc_z.jpg']
+const imgUrl = computed(()=>{
+  const width = document.body.clientWidth;
+  if(width < 1000){
+    return imgUrls[0]
+  }
+  return imgUrls[1] 
+})
 </script>
 
 <template>
   <h1>{{ msg }}</h1>
-
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
-  </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Learn more about IDE Support for Vue in the
-    <a
-      href="https://vuejs.org/guide/scaling-up/tooling.html#ide-support"
-      target="_blank"
-      >Vue Docs Scaling up Guide</a
-    >.
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+  <img :src="imgUrl" alt="">
 </template>
 
 <style scoped>
